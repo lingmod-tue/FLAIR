758528d84d5d77ffb6f34fcb3929f9e9a225fffc
Madeesh Kannan <shadeMe@users.noreply.github.com>
2016-05-12 09:09:27 +0000

    Update license headers
    Former-commit-id: 65e8c20154881b89b1f619c76a43706f93b158de

--- src/java/com/flair/testing/LocalFileParsingTest.java
+++ src/java/com/flair/testing/LocalFileParsingTest.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+ 
  */
 package com.flair.testing;
 
--- web/js/flair-core.js
+++ web/js/flair-core.js
@@ -1,3 +1,8 @@
+/*
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+ */
+
 //============= FLAIR - Root namespace ================//
 var FLAIR = FLAIR || {};
  
